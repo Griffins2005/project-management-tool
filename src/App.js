f8f7e803c7c773c89eb4f0659c98eeabf25697c6
Griffins2005 <Griffins2005>
2025-02-05 00:49:22 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Project from "./pages/project";
@@ -13,22 +13,20 @@ function App() {
   const location = useLocation();
 
   return (
-    <Router>
-      <div className="App">
-        {/* Hide Navbar and Footer on Login Page */}
-        {location.pathname !== "/" && <Navbar />}
-        <main>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/team" element={<Teams />} />
-            <Route path="/progress" element={<ProgressTracking />} />
-            <Route path="/priority" element={<Priority />} />
-          </Routes>
-        </main>
-        {location.pathname !== "/" && <Footer />}
-      </div>
-    </Router>
+    <div className="App">
+      {/* Hide Navbar and Footer on Login Page */}
+      {location.pathname !== "/" && <Navbar />}
+      <main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/team" element={<Teams />} />
+          <Route path="/progress" element={<ProgressTracking />} />
+          <Route path="/priority" element={<Priority />} />
+        </Routes>
+      </main>
+      {location.pathname !== "/" && <Footer />}
+    </div>
   );
 }
 
